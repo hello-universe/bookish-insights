3017a64ac2fb892d239fa398a8d37af55b3c2727
hello-universe <amitverma.av1722@gmail.com>
2024-07-16 23:23:26 +0530

    Initial commit with header completed

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Components/Header'
+
+function App() {
+  return (
+    <div className='app p-4 md:px-8 bg-[#C1E7FF] h-screen'>
+      <Header />
+      
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/Components/Header.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+function toggleMobileMenu(){
+    const menu =  document.getElementById("menu");
+    menu.classList.toggle("hidden")
+    menu.classList.toggle("flex")
+}
+function Header() {
+  return (
+    <div className="header flex relative justify-between items-center">
+      <a
+        href="#"
+        className="logo-holder flex items-center justify-center gap-2"
+      >
+        <div className="logo w-10 h-10 rounded-[50%] bg-[#22577a] text-white text-xl flex items-center justify-center">
+          B
+        </div>
+        <div className="logo-text text-md font-semibold">Bookish Insights</div>
+      </a>
+      <nav>
+        <ul id="menu" className="absolute right-0 top-8 text-lg hidden flex-col justify-center items-end gap-4 sm:flex sm:top-0 sm:flex-row sm:gap-16 sm:items-center">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#books">Books</a>
+          </li>
+          <li>
+            <a href="#categories">Categories</a>
+          </li>
+          <li className="user-logo w-10 h-10 rounded-[50%] bg-purple-600 text-white text-xl flex justify-center items-center">
+            A
+          </li>
+        </ul>
+      <a href="#" className="mobile-toggle sm:hidden" onClick={toggleMobileMenu}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 -960 960 960"
+          width="24"
+        >
+          <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+        </svg>
+      </a>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
